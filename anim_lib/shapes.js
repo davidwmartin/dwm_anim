08efd9cda257a149ebb14624110f2a4daf21cc9f
David Martin <david.w.martin@me.com>
2017-04-29 19:16:07 -0700

    building out animations library

--- anim_lib/shapes.js
+++ anim_lib/shapes.js
@@ -1,1 +1,9 @@
-var s = {};
+var s = module.exports = {};
+
+s.line = function (context, x1, y1, x2, y2){
+ context.beginPath();
+ context.moveTo(x1,y1);
+ context.lineTo(x2,y2);
+ context.stroke();
+ context.beginPath();
+};
--- anim_lib/utility.js
+++ anim_lib/utility.js
@@ -1,7 +1,7 @@
 var u = module.exports = {};
 
 /***
-** Random Number Generators
+** Math Functions
 ***/
 
 // random float
@@ -17,24 +17,53 @@ u.randInt = function randInt(low, high){
 
 // (1 or -1)
 u.posNeg = function posNeg(){
- return randInt(0,1) * 2 - 1;
+ return u.randInt(0,1) * 2 - 1;
+}
+
+function clamp(value, min, max){
+  return Math.min(Math.max(value, Math.min(min, max)),Math.max(min, max));
 }
 
 /***
 ** Color Functions
 ***/
 
-// u.rgb = function rgb(r,g,b){
-// 	if (g == undefined) g = r;
-// 	if (b == undefined) b = r;
-// 	return 'rgb('+clamp(Math.round(r),0,255)+', '+clamp(Math.round(g),0,255)+', '+clamp(Math.round(b),0,255)+')';
-// }
+u.rgb = function rgb(r,g,b){
+	if (g == undefined) g = r;
+	if (b == undefined) b = r;
+	return 'rgb('+clamp(Math.round(r),0,255)+', '+clamp(Math.round(g),0,255)+', '+clamp(Math.round(b),0,255)+')';
+}
+u.rgba = function rgba(r, g, b, a) {
+  if (g == undefined) {
+   return 'rgb('+clamp(Math.round(r),0,255)+', '+clamp(Math.round(r),0,255)+', '+clamp(Math.round(r),0,255)+')';
+ } else if (b == undefined) {
+    return 'rgba('+clamp(Math.round(r),0,255)+', '+clamp(Math.round(r),0,255)+', '+clamp(Math.round(r),0,255)+', '+clamp(g,0,1)+')';
+  } else if (a == undefined){
+  return 'rgba('+clamp(Math.round(r),0,255)+', '+clamp(Math.round(g),0,255)+', '+clamp(Math.round(b),0,255)+', 1)';
+} else {
+return 'rgba('+clamp(Math.round(r),0,255)+', '+clamp(Math.round(g),0,255)+', '+clamp(Math.round(b),0,255)+', '+clamp(a,0,1)+')';
+ }
+};
+
+u.background = function background(context, r, g, b, a){
+ if (g == undefined) {
+ context.fillStyle = u.rgb(r, r, r);
+ } else if (b == undefined && a == undefined) {
+  context.fillStyle = u.rgba(r, r, r, g);
+ } else if (a == undefined) {
+  context.fillStyle = u.rgb(r, g, b);
+ } else {
+ context.fillStyle = u.rgba(r, g, b, a);
+ }
+ // TODO -- better solution for w and h here
+ context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+};
 
 /***
 ** Movement Functions
 ***/
 
-u.bounce = function bounce(num, min, max, sz) {
+u.bounce = function (num, min, max, sz) {
   if (sz === undefined) {
     sz = 0;
   }
@@ -43,4 +72,10 @@ u.bounce = function bounce(num, min, max, sz) {
   } else {
     return 0;
   }
+}
+
+// get distance between two sets of x,y  coordinates
+u.dist = function dist(x1, y1, x2, y2) {
+ x2-=x1; y2-=y1;
+ return Math.sqrt((x2*x2) + (y2*y2));
 }
